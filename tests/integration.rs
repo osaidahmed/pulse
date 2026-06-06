c1a36cd91f807b1fddb99b47eb0620dc80aa870b
Omar Saidahmed <osaidahm@uwaterloo.ca>
2026-06-06 17:43:10 -0400

    guard architecture-class smell decoupling with regression tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -81,6 +81,7 @@ mod history_common;
 #[path = "integration/audit_components.rs"] mod audit_components;
 #[path = "integration/audit_arch_smells.rs"] mod audit_arch_smells;
 #[path = "integration/audit_centrality.rs"] mod audit_centrality;
+#[path = "integration/audit_decoupling.rs"] mod audit_decoupling;
 #[path = "integration/audit_parallel_inheritance.rs"] mod audit_parallel_inheritance;
 #[path = "integration/audit_parallel_inheritance_extended.rs"] mod audit_parallel_inheritance_extended;
 #[path = "integration/audit_per_language_deep.rs"] mod audit_per_language_deep;
